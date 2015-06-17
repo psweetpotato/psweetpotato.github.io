f79c255cdda2dcdf34c6e173540b5d20f91799ba
psweetpotato <p.sweetpotato@gmail.com>
2015-06-17 17:04:49 -0400

    creates alert on window resize

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
+var $window = $(window);
+var $windowSize = $window.width()
+
 function contactFunction() {
-    $('html,body').animate({
-        scrollTop: $(".wrapper").offset().top},
-        'slow');
+  $('html,body').animate({
+      scrollTop: $(".wrapper").offset().top},
+      'slow');
 };
 
 function topPage() {
-    $('html,body').animate({
-        scrollTop: $("#am").offset().top},
-        'slow');
+  $('html,body').animate({
+      scrollTop: $("#am").offset().top},
+      'slow');
+};
+
+window.onresize = function(event){
+  var newSize = $window.width();
+    if (newSize < $windowSize * 0.35) {
+      alert("YUP, it's responsive!")
+    };
 };
